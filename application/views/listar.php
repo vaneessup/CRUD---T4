59f31e0a4a70b93d3baafb76e388b398c5be676e
Vanessa Ramirez <vanessaramirez.vr45@gmail.com>
2022-08-21 21:28:22 -0600

    version1

---
+++ application/views/listar.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    ><head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title> Document Title </title>
+
+    </head>
+<body>
+        Lista 
+</body>
+</html>
